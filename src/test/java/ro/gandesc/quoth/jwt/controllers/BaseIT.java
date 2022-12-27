26f2023eaefa899c702aa4332b2818c5efd89398
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2022-12-27 18:18:47 +0200

    wip

---
+++ src/test/java/ro/gandesc/quoth/jwt/controllers/BaseIT.java
@@ -0,0 +1,25 @@
+package ro.gandesc.quoth.jwt.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+public abstract class BaseIT {
+
+    @Autowired
+    WebApplicationContext wac;
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(wac)
+                .apply(springSecurity())
+                .build();
+    }
+}
---
+++ src/test/java/ro/gandesc/quoth/jwt/controllers/ProductControllerTest.java
@@ -0,0 +1,15 @@
+package ro.gandesc.quoth.jwt.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductControllerTest extends BaseIT {
+
+    @Test
+    public void getProducts() throws Exception {
+
+
+        //
+    }
+}
