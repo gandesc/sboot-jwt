f52bb7fdc4075390c22e4622ea02c775109fd7d5
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2023-01-05 17:42:33 +0200

    fixed test methods naming

--- src/test/java/ro/gandesc/jwt/web/controllers/UserControllerIT.java
+++ src/test/java/ro/gandesc/jwt/web/controllers/UserControllerIT.java
@@ -1,16 +1,10 @@
 package ro.gandesc.jwt.web.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
-import ro.gandesc.jwt.domain.Product;
-
-import java.util.Random;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -23,13 +17,13 @@ public class UserControllerIT extends BaseIT {
     class ListProducts {
         @Test
         @WithUserDetails("admin")
-        public void getProductsADMIN() throws Exception {
+        public void getUserADMIN() throws Exception {
             mockMvc.perform(get("/user"))
                     .andExpect(status().isOk());
         }
 
         @Test
-        public void getProductsNOAUTH() throws Exception {
+        public void getUserNOAUTH() throws Exception {
             mockMvc.perform(get("/user"))
                     .andExpect(status().isUnauthorized());
         }
