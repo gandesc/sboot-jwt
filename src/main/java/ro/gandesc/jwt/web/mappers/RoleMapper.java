3798efe1f41244dea1e1bd3b254f36310dfd7839
Mihai Ocneanu <mihai.ocneanu@gmail.com>
2023-01-10 10:45:35 +0200

    finished role mapper impl; tests pass

--- src/main/java/ro/gandesc/jwt/web/mappers/RoleMapper.java
+++ src/main/java/ro/gandesc/jwt/web/mappers/RoleMapper.java
@@ -1,8 +1,10 @@
 package ro.gandesc.jwt.web.mappers;
 
+import org.mapstruct.Mapper;
 import ro.gandesc.jwt.domain.security.Role;
 import ro.gandesc.jwt.web.models.RoleDto;
 
+@Mapper
 public interface RoleMapper {
     RoleDto roleToRoleDto(Role entity);
 
